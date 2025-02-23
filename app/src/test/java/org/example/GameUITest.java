0ae35f36c1568dc7dfc5c707a3cdd8c9a96c8964
Christian165293 <christian.bourquin05@gmail.com>
2025-02-22 20:48:09 -0600

    Tests completed and all is working

--- app/src/test/java/org/example/GameUITest.java
+++ app/src/test/java/org/example/GameUITest.java
@@ -2,6 +2,24 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class GameUITest {
 
+    @Test
+    public void testGetBoard() {
+        GameUI gameUI = new GameUI();
+        String[][] board = gameUI.getBoard();
+
+        assertNotNull(board);
+        assertEquals(3, board.length);
+        assertEquals(3, board[0].length);
+    }
+
+    @Test
+    public void testPrintBoard() {
+        String[][] board = {{"  1  ", "  2  ", "  3  "}, {"  4  ", "  5  ", "  6  "}, {"  7  ", "  8  ", "  9  "}};
+
+        GameUI.printBoard(board);
+    }
 }
--- app/src/test/java/org/example/TTTRoundTest.java
+++ app/src/test/java/org/example/TTTRoundTest.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TTTRoundTest {
 
+    private String[][] board;
+
+    @BeforeEach
+    public void setUp() {
+        board = new String[][]{{"  1  ", "  2  ", "  3  "}, {"  4  ", "  5  ", "  6  "}, {"  7  ", "  8  ", "  9  "}};
+    }
+
+    @Test
+    public void testComputerTurn() {
+        TTTRound.computerTurn(board);
+        boolean moveMade = false;
+
+        for (String[] row : board) {
+            for (String cell : row) {
+                if (cell.equals("  O  ")) {
+                    moveMade = true;
+                    break;
+                }
+            }
+        }
+
+        assertTrue(moveMade);
+    }
+
+    @Test
+    public void testPlayerTurn() {
+        Scanner scanner = new Scanner("1\n");
+        TTTRound.playerTurn(board, scanner);
+        assertEquals("  X  ", board[0][0]);
+    }
 }
